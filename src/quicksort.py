97957265540c1646f60b73caf744f44e54931b83
SangDoo Nam <kexe009@hotmail.com>
2023-10-25 21:42:10 +0200

    finish

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,3 +1,21 @@
 # TODO implement quicksort
-def quicksort(list: list) -> list:
-    pass
+def quicksort(list: list, low=0, high=-1) -> list:
+    if high == -1:
+        high = len(list) - 1
+        
+    if low < high:
+        pi = partition(list, low, high)
+        quicksort(list, low, pi -1)
+        quicksort(list, pi + 1, high)
+        return list
+
+def partition(list, low, high):
+    pivot = list[high]
+    i = low - 1
+    for j in range(low, high):
+        if list[j] <= pivot:
+            i += 1
+            list[i], list[j] =  list[j], list[i]
+    (list[i + 1], list[high]) = (list[high], list[i + 1])
+    
+    return i + 1
